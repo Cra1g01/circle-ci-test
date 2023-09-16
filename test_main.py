99bab9abe3ba09074f358eff6551ddb0ff6e5547
Craig <58040967+Cra1g01@users.noreply.github.com>
2023-09-16 21:59:15 +0100

    feat: sub func and test_sub

--- test_main.py
+++ test_main.py
@@ -1,4 +1,13 @@
-from main import add
+from main import add, sub
 
-def test_main():
+
+def test_add():
     assert add(1, 1) == 2
+    assert add(2, -3) == -1
+    assert add(5.5, 2) == 7.5
+
+
+def test_sub():
+    assert sub(10, 2) == 8
+    assert sub(10, 20) == -10
+    assert sub(-5, -5) == 0
